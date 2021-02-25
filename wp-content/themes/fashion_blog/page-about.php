b0b04e0b58149e9815fd951efc91c49a4fcced42
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-02-25 13:23:45 +0200

    upldate

--- wp-content/themes/fashion_blog/page-about.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div class="about-page">
-    <h1>We <i class="fas fa-heart"></i> fashion !</h1>
-    <p>We try our best to share high quality content related to latest style updates. We are a community of fashion fashion lovers.</p>
-    <span>Keep in touch with us!</span>
-</div>
-
-<?php get_footer(); ?>
